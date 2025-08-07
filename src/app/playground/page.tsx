88ce6562db038700a931ca79bf460d586eda05ca
Pavol Hejny <me@pavolhejny.com>
2025-08-07 12:22:23 +0200

    Use `<TextareaWithCounter/>` in playground

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,34 +1,9 @@
-import { AgentBookEditor } from '@/components/book-editor/src';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type TODO_any = any;
+import TextareaWithCounter from '@/components/textarea-with-counter/TextareaWithCounter';
 
 export default function PlaygroundPage() {
     return (
         <div>
-            <AgentBookEditor
-                mode="edit"
-                initialAgentSource={'xxx' as TODO_any}
-                onSubmit={(agentSource: TODO_any /* string_agent_source */) => {
-                    console.log('onSubmit', agentSource);
-                }}
-                onClose={() => {
-                    console.log('onClose');
-                    return null as TODO_any;
-                }}
-                onExport={() => {
-                    console.log('onExport');
-                    return null as TODO_any;
-                }}
-                abTestText={{
-                    singular: 'xxx1',
-                    singularCapitalized: 'xxx2',
-                    createCapitalized: 'xxx3',
-                }}
-                backgroundImage={'xxx' as TODO_any}
-                className="agent-book-editor"
-                prefillAgentSource={'xxx' as TODO_any}
-            />
+            <TextareaWithCounter placeholder="Start typing..." maxLength={500} rows={4} className="mb-4" />
         </div>
     );
 }
